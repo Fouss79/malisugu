daf3e240e9e0953fa9cdbe3541e94644581f35c0
Fousseny TRAORE <foussenytraore692@gmail.com>
2026-09-04 14:55:17 +0000

    Permission

--- app/dashboard/admin/[role]/page.jsx
+++ app/dashboard/admin/[role]/page.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
-import { useAuth } from '../../../context/AuthContext'
+"use client";
 
-const page = () => {
-  const { user } = useAuth();
+import React from "react";
+import { useAuth } from "../../../context/AuthContext";
+
+const Page = () => {
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return <div>Chargement...</div>;
+  }
+
+  if (!user) {
+    return <div>Utilisateur non connecté</div>;
+  }
+
   return (
-    <div>Dashboard user.role</div>
-  )
-}
+    <div>
+      <h1>Dashboard {user.role}</h1>
 
-export default page
+      <p>
+        Bienvenue, {user.email}
+      </p>
+    </div>
+  );
+};
+
+export default Page;
